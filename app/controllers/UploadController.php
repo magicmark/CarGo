528f5013e408bbd94029a18080100ffe70fed31a
magicmark <mark@larah.me>
2014-02-08 17:01:24 +0100

    Added upload code

---
+++ app/controllers/UploadController.php
@@ -0,0 +1,40 @@
+<?php
+
+class UploadController extends \Phalcon\Mvc\Controller
+{
+
+	private $validImageTypes = array(
+		'image/jpeg',
+		'image/png',
+		'image/gif'
+		);
+
+	public function indexAction()
+	{
+
+		//var_dump($this->request($this->authURL));
+	}
+
+	public function carAction() {
+
+    if ($this->request->hasFiles() != true) {
+    	echo json_encode(array('error' => 'No image uploaded!'));
+    	return;
+    }
+
+    // We only want the first file. Gracefully ignore any extra files as that would be errant behaviour.
+    $file = reset($this->request->getUploadedFiles());
+ 		
+ 		$type = ($file->getRealType() != '') ? $file->getRealType() : $file->getType();
+
+ 		if (!in_array($type, $this->validImageTypes)) {
+ 			echo json_encode(array('error' => 'Image type not allowed!'));
+ 			return;
+ 		}
+
+ 		$name = md5(microtime() . 'Cars.com rule!' . mt_rand()).'.'.pathinfo($file->getName(), PATHINFO_EXTENSION);
+
+ 		$file->moveTo('/var/www/cargo/plates/' . $name);
+
+	}
+}
