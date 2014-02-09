897949e34ceb2d7db0c7b8aec78960906b7b779e
magicmark <mark@larah.me>
2014-02-09 01:47:37 +0100

    fixed path

--- app/library/PlatesNumber.php
+++ app/library/PlatesNumber.php
@@ -10,7 +10,7 @@ class PlatesNumber extends Component
   public function getPlateNumbers( $image = null )
   {
     exec("/Public/openalpr/src/alpr ". $image ." -r /Public/openalpr/runtime_data/", $output);
-    $plateNumberTemp = [];
+    $plateNumberTemp = array();
     for( $i = 0; $i < count($output); $i++)
       preg_match("/^[ ]*\- ([A-za-z0-9]*)/", $output[$i], $plateNumberTemp[$i-1] );
 
