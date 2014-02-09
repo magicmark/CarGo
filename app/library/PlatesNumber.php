0edfd1d13b23b33d0ae1396b5ea4cc990147bc34
Jessie Abramson <jessie.abramson@hotmail.fr>
2014-02-09 03:02:05 +0000

    More

--- app/library/PlatesNumber.php
+++ app/library/PlatesNumber.php
@@ -7,13 +7,57 @@ class PlatesNumber extends Component
   public $plateNumber;
   public $image;
 
+  private function swap( $input, $index )
+  {
+    for( $i = $index; $i < count($input); $i++ )
+    {
+      switch ($input[$i]) {
+        case '0':
+          $result1 = swap($input, $i+1);
+          $input[$i] = 'O';
+          $result2 = swap($input, $i+1);
+          return array_merge(result1, result2)
+          break;
+        
+        case 'O':
+          $result1 = swap($input, $i+1);
+          $input[$i] = '0';
+          $result2 = swap($input, $i+1);
+          return array_merge(result1, result2)
+          break;
+
+        case '5':
+          $result1 = swap($input, $i+1);
+          $input[$i] = 'S';
+          $result2 = swap($input, $i+1);
+          return array_merge(result1, result2)
+          break;
+
+        case 'S':
+          $result1 = swap($input, $i+1);
+          $input[$i] = '5';
+          $result2 = swap($input, $i+1);
+          return array_merge(result1, result2)
+          break;
+
+        default:
+          break;
+      }
+    }
+    return $input;
+  }
+
   public function getPlateNumbers( $image = null )
   {
     exec("/home/steve/plateRecognition/openalpr/src/alpr ". $image ."  -r /home/steve/plateRecognition/openalpr/runtime_data/", $output1);
     exec("/home/steve/plateRecognition/openalpr/src/alpr ". $image ." -c eu -r /home/steve/plateRecognition/openalpr/runtime_data/", $output2);
     exec("/home/steve/plateRecognition/openalpr/src/alpr ". $image ." -c us -r /home/steve/plateRecognition/openalpr/runtime_data/", $output3);
     
-    $output = array_merge($output1,$output2,$output3);
+    $outputPre = array_merge($output1, $output2,$output3);
+
+    $output = array();
+    for( $i = 0; $i < count($outputPre); $i++)
+      array_merge($output, swap($outputPre, 0));
     
     $plateNumberTemp = array();
 
