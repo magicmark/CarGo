e2c76eb8a56a8aadff425deaaa8ed85f7977eb31
Jessie Abramson <jessie.abramson@hotmail.fr>
2014-02-09 03:53:32 +0000

    More

--- app/library/PlatesNumber.php
+++ app/library/PlatesNumber.php
@@ -11,7 +11,7 @@ class PlatesNumber extends Component
   {
     for( $i = $index; $i < count($input); $i++ )
     {
-      echo $input[$i] . " " . count($input) . " ";
+      echo $input[$i] . " " . count($input) . " " . $input . " END " ;
       switch ($input[$i]) {
         case "0":
           $result1 = $this->swap($input, $i+1);
