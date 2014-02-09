b29102b8e804752733176098d6fcd9cbeb38c522
Jessie Abramson <jessie.abramson@hotmail.fr>
2014-02-09 03:27:19 +0000

    More

--- app/library/PlatesNumber.php
+++ app/library/PlatesNumber.php
@@ -69,6 +69,7 @@ class PlatesNumber extends Component
     for( $i = 0; $i < count($output); $i++)
       preg_match("/^[ ]*\- ([A-za-z0-9]*)/", $output[$i], $plateNumberTemp[$i-1] );
 
+    $this->plateNumber = array();
     for( $i = 0; $i < count($plateNumberTemp); $i++)
       if( isset($plateNumberTemp[$i][1]))
         // $this->plateNumber[] = $plateNumberTemp[$i][1];
